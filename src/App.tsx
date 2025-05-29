6cc12936b2448ae8792f47666925ea72d4ebc1e2
meisara <148285560+b20llo-o@users.noreply.github.com>
2025-05-29 11:41:28 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,33 @@
-import { Routes, Route } from "react-router-dom"
-import Layout from "./components/Layout"
-import HomePage from "./pages/HomePage"
-import PoemsPage from "./pages/PoemsPage"
-import PoemDetailPage from "./pages/PoemDetailPage"
-import ThoughtsPage from "./pages/ThoughtsPage"
-import ThoughtDetailPage from "./pages/ThoughtDetailPage"
-import AboutPage from "./pages/AboutPage"
-import SupportPage from "./pages/SupportPage"
-import ContactPage from "./pages/ContactPage"
-import NotFound from "./pages/NotFound" // Import the NotFound component
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import HomePage from './pages/HomePage';
+import PoemsPage from './pages/PoemsPage';
+import PoemDetailPage from './pages/PoemDetailPage';
+import ThoughtsPage from './pages/ThoughtsPage';
+import ThoughtDetailPage from './pages/ThoughtDetailPage';
+import AboutPage from './pages/AboutPage';
+import SupportPage from './pages/SupportPage';
+import ContactPage from './pages/ContactPage';
+import ScrollToTopButton from './components/ScrollToTopButton';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<HomePage />} />
-        <Route path="poems" element={<PoemsPage />} />
-        <Route path="poems/:id" element={<PoemDetailPage />} />
-        <Route path="thoughts" element={<ThoughtsPage />} />
-        <Route path="thoughts/:id" element={<ThoughtDetailPage />} />
-        <Route path="about" element={<AboutPage />} />
-        <Route path="support" element={<SupportPage />} />
-        <Route path="contact" element={<ContactPage />} />
-
-        {/* Add this catch-all route at the end */}
-        <Route path="*" element={<NotFound />} />
-      </Route>
-    </Routes>
-  )
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="poems" element={<PoemsPage />} />
+          <Route path="poems/:id" element={<PoemDetailPage />} />
+          <Route path="thoughts" element={<ThoughtsPage />} />
+          <Route path="thoughts/:id" element={<ThoughtDetailPage />} />
+          <Route path="about" element={<AboutPage />} />
+          <Route path="support" element={<SupportPage />} />
+          <Route path="contact" element={<ContactPage />} />
+        </Route>
+      </Routes>
+      <ScrollToTopButton />
+    </>
+  );
 }
 
-export default App
+export default App;
